5be1c9332e40f48b91d8cd121827f04340bea8ae
Ulisses Batista Santana <santana.gru@gmail.com>
2025-06-02 20:42:41 -0300

    Projetando sistema de pedidos online

---
+++ src/main/java/sistema_pedido_online_app/SistemaDePedidoOnLineApplication.java
@@ -0,0 +1,13 @@
+package sistema_pedido_online_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePedidoOnLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePedidoOnLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sistema_pedido_online_app/SistemaDePedidoOnLineApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema_pedido_online_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePedidoOnLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
